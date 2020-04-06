a677575a69f54996c8dc288b706819afcf7f8b30
Alek Rifai <alekrifai97@gmail.com>
2020-04-06 22:33:01 +0300

    Inheritance

---
+++ inheritance/InheritanceExercise/Zoo/Mammal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Mammal : Animal
+    {
+        public Mammal(string name)
+            : base(name)
+        {
+
+        }
+    }
+}
